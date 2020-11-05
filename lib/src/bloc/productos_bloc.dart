b4a59bdbb263b8f43c58ff683f38dd06d9037353
andres velasco <velasco.andrs@gmail.com>
2020-11-04 23:36:46 -0500

    productos bloc

---
+++ lib/src/bloc/productos_bloc.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:flup/src/models/producto_model.dart';
+import 'package:flup/src/providers/productos_provider.dart';
+import 'package:rxdart/rxdart.dart';
+
+class ProductosBloc {
+  final _productosController = new BehaviorSubject<List<ProductoModel>>();
+  final _cargandoController = new BehaviorSubject<bool>();
+
+  final _productosProvider = new ProductosProvider();
+
+  Stream<List<ProductoModel>> get productosStream =>
+      _productosController.stream;
+  Stream<bool> get cargandoStream => _cargandoController.stream;
+
+  void cargarProductos() async {
+    final productos =
+        await _productosProvider.findAll(ProductoModel.fromJsonToModel);
+    _productosController.sink.add(productos);
+  }
+
+  void agregarProducto(ProductoModel producto) async {
+    _cargandoController.sink.add(true);
+    await _productosProvider.create(producto);
+    _cargandoController.sink.add(false);
+  }
+
+  void editarProducto(ProductoModel producto) async {
+    _cargandoController.sink.add(true);
+    await _productosProvider.update(producto.id, producto);
+    _cargandoController.sink.add(false);
+  }
+
+  void borrarProducto(String id) async {
+    _cargandoController.sink.add(true);
+    await _productosProvider.delete(id);
+    _cargandoController.sink.add(false);
+  }
+
+  Future<String> subirFoto(File foto) async {
+    _cargandoController.sink.add(true);
+    final fotoUrl = await _productosProvider.subirImagen(foto);
+    _cargandoController.sink.add(false);
+    return fotoUrl;
+  }
+
+  void dispose() {
+    _productosController.close();
+    _cargandoController.close();
+  }
+}
